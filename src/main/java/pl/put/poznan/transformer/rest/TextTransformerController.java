a494d2948d05f41c0718481f72a8d67aa967a00d
arabica <mg96272@gmail.com>
2020-12-11 12:29:19 +0100

    Basic API, test GET and POST methods, classes for Locations

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -2,18 +2,20 @@ package pl.put.poznan.transformer.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.Pomieszczenie;
+import pl.put.poznan.transformer.logic.Request;
+import pl.put.poznan.transformer.logic.Result;
 import pl.put.poznan.transformer.logic.TextTransformer;
 
 import java.util.Arrays;
 
-
 @RestController
-@RequestMapping("/{text}")
+//@RequestMapping("/a/{text}")
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
-
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    //@RequestMapping("/a/{text}")
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json", value = "/a/{text}")
     public String get(@PathVariable String text,
                               @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
@@ -26,7 +28,7 @@ public class TextTransformerController {
         return transformer.transform(text);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json", value = "/a/{text}")
     public String post(@PathVariable String text,
                       @RequestBody String[] transforms) {
 
@@ -40,7 +42,31 @@ public class TextTransformerController {
     }
 
 
+        @RequestMapping(method = RequestMethod.POST, value = "/math")
+        @ResponseBody
+        public Result math(@RequestBody final Request request) {
+            final Result result = new Result();
+            result.setAddition(request.getLeft() + request.getRight());
+            result.setSubtraction(request.getLeft() - request.getRight());
+            result.setMultiplication(request.getLeft() * request.getRight());
+            return result;
+        }
 
-}
+
+    //@RequestMapping("/a/{text}")
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json", value = "/get_area/{id}")
+    public String get_area(@PathVariable String id) {
+        final Pomieszczenie room = new Pomieszczenie();
+        room.setId(id);
+        // log the parameters
+        return room.getId();
+    }
+
+    }
+
+
+
+
+
 
 
